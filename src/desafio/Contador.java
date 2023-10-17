98f77c182faeed3ad0b724432e5f7361a8a56639
JVPCoder <doublecanal@gmail.com>
2023-10-17 16:46:06 -0300

    FIx

--- src/desafio/Contador.java
+++ src/desafio/Contador.java
@@ -36,7 +36,7 @@ public class Contador {
 		//realizar o for para imprimir os números com base na variável contagem
 		
 		for(int i=0;i<contagem; i++) {
-			System.out.println("Imprimindo numero " + (i+1));
+			System.out.println("Imprimindo o número " + (i+1));
 		}
 	}
 }
